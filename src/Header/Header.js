18f7d01da5e3637387e90ba7cfe3d3d699411641
GarrettAudet <70986283+GarrettAudet@users.noreply.github.com>
2024-07-05 18:47:23 -0400

    added structure for the left header

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -4,9 +4,14 @@ import './Header.css'
 function Header () {
     return (
         <div className="header">
-            <div className="header-left">garrett audet</div>
+            <div className="header-left">
+                <span className="first-name">garrett</span>
+                <span className="collapse-on-hover"> </span>
+                <span className="last-name">audet</span>
+                <span className="logo-letter-hide delay-75">.</span>
+                <span className="logo-letter-hide delay-100">com</span>
+            </div>
             <div className="header-right">
-                <div>Header</div>
                 <div>Projects</div>
                 <div>Writing</div>
                 <div>Contact</div>
