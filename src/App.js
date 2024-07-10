ecef95b370658e2a5a6f339b5f1ae9f7d941ca05
GarrettAudet <70986283+GarrettAudet@users.noreply.github.com>
2024-07-09 23:07:53 -0400

    Fixed header positioning

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './Header/Header'
+import Carousal from './Carousal/carousal'
 
 function App() {
   return (
     <div className="background">
         <Header />
+        <Carousal />
     </div>
   );
 }
---
+++ src/Carousal/carousal.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './carousal.css'
+
+function Carousal () {
+    return (
+        <div className="carousal-container">
+            <div className = "gallery">
+                <img src='../Img/personalImage.jpg' alt =""></img>
+                <img src="" alt =""></img>
+                <img src="" alt =""></img>
+                <img src="" alt =""></img>
+            </div>
+        </div>
+    );
+}
+
+export default Carousal
