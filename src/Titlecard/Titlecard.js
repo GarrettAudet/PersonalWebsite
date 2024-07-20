5db331c73ad2abbd1ac4d17c12f594ffe3621b89
GarrettAudet <70986283+GarrettAudet@users.noreply.github.com>
2024-07-20 03:03:27 -0400

    Added typing text animation

--- src/Titlecard/Titlecard.js
+++ src/Titlecard/Titlecard.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import { TypeAnimation } from 'react-type-animation';
 import './Titlecard.css';
 
 const TitleCard = () => {
@@ -7,8 +8,19 @@ const TitleCard = () => {
       <div className="header-container">
         <h2>
           <span className="inline-block">I'm a developer and business practitioner who believes</span>
-          <span className="inline-block">that <span className="text-decoration"> bridging perspectives</span> unlocks business opportunities
-          </span>
+          <span className="inline-block">that <span className="text-decoration"> bridging perspectives </span> facilitates <span className="text-animation">
+            <TypeAnimation
+              sequence={[
+                'economic potential.', 3000,
+                'professional success.', 3000,
+                'individual growth.', 3000
+              ]}
+              wrapper="span"
+              speed={10}
+              style={{ fontSize: '1em', display: 'inline-block' }}
+              repeat={Infinity}
+            />
+          </span></span>
         </h2>
       </div>
     </a>
