fddd7f99f492081cad0a9155f4090347b3fea69f
GarrettAudet <70986283+GarrettAudet@users.noreply.github.com>
2024-07-27 07:05:06 -0400

    Added text animation for top left of the header

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,24 +1,58 @@
-import React from 'react';
-import './Header.css'
+import React, { useEffect } from 'react';
+import './Header.css';
 
-function Header () {
+function Header() {
+    useEffect(() => {
+        const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        let interval = null;
+        let isOriginal = true;
+
+        const firstNameElement = document.querySelector(".first-name");
+
+        firstNameElement.onmouseover = event => {
+            let iteration = 0;
+            const originalName = "garrett audet";
+            const fullName = "garrettaudet.com";
+            const targetText = isOriginal ? fullName : originalName;
+
+            clearInterval(interval);
+            interval = setInterval(() => {
+                event.target.innerText = targetText
+                    .split("")
+                    .map((letter, index) => {
+                        if (index < iteration) {
+                            return targetText[index];
+                        }
+                        return letters[Math.floor(Math.random() * 26)];
+                    })
+                    .join("");
+
+                if (iteration >= targetText.length) {
+                    clearInterval(interval);
+                    isOriginal = !isOriginal;
+                }
+                iteration += 1 / 3;
+            }, 30);
+        };
+
+        return () => clearInterval(interval); // Clean up interval on component unmount
+    }, []);
+
     return (
         <div className="header">
             <div className="header-left">
-                <span className="first-name">garrett</span>
-                <span className="collapse-on-hover"> </span>
-                <span className="last-name">audet</span>
-                <span className="logo-letter-hide delay-75">.</span>
-                <span className="logo-letter-hide delay-100">com</span>
+                <span className="first-name" data-value="garrettaudet.com">garrett audet</span>
             </div>
             <div className="header-right">
                 <div>About</div>
                 <div>Experience</div>
-                <a href="https://garrett-audet.medium.com/">Writing </a>
+                <a href="https://garrett-audet.medium.com/">Writing</a>
                 <div>Contact</div>
             </div>
-      </div>
+        </div>
     );
 }
 
-export default Header
+export default Header;
+
+
