4bda522edcfb7fd6cc3b20eaebe613e1b54e31db
GarrettAudet <70986283+GarrettAudet@users.noreply.github.com>
2024-07-28 20:57:20 -0400

    Fixed <a> tags without href

--- src/Hover-Icon/Hover.js
+++ src/Hover-Icon/Hover.js
@@ -4,13 +4,13 @@ import './Hover.css';
 function Hover({ contactLogo }) {
     return (
         <div className="socialContacts">
-            <a href = "#" title = "Github" className = "link">
+            <a href = "https://www.google.com/" title = "Github" className = "link">
                 <i className = "bi bi-youtube">x</i>
             </a>
-            <a href = "#" title = "LinkedIn" className = "link">
+            <a href = "https://www.google.com/" title = "LinkedIn" className = "link">
                 <i className = "bi bi-linkedin">y</i>
             </a>
-            <a href = "#" title = "Medium" className = "link">
+            <a href = "https://www.google.com/" title = "Medium" className = "link">
                 <i className = "bi bi-medium">z</i>
             </a>
         </div>
