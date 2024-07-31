af857e5f7cfb8ea3656f766c719face1196bfcb1
GarrettAudet <70986283+GarrettAudet@users.noreply.github.com>
2024-07-31 18:28:39 -0400

    Updated Hover Icons

--- src/Hover-Icon/Hover.js
+++ src/Hover-Icon/Hover.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import './Hover.css';
+import { ReactComponent as GithubLogo } from '../Img/github.svg';
+import { ReactComponent as LinkedinLogo } from '../Img/linkedin.svg';
+import { ReactComponent as MediumLogo } from '../Img/medium.svg';
 
-function Hover({ contactLogo }) {
+function Hover() {
     return (
         <div className="socialContacts">
             <a href = "https://www.google.com/" title = "Github" className = "link">
-                <i className = "bi bi-youtube">x</i>
+                <GithubLogo className = "bi bi-github" />
             </a>
             <a href = "https://www.google.com/" title = "LinkedIn" className = "link">
-                <i className = "bi bi-linkedin">y</i>
+                <LinkedinLogo className = "bi bi-linkedin"/>
             </a>
             <a href = "https://www.google.com/" title = "Medium" className = "link">
-                <i className = "bi bi-medium">z</i>
+                <MediumLogo className = "bi bi-medium"/>
             </a>
         </div>
     );
