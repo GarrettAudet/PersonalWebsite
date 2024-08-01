309995a6f0802a30d4a0b0d1041139bc8c8f5e19
GarrettAudet <70986283+GarrettAudet@users.noreply.github.com>
2024-07-31 23:39:15 -0400

    Updated animations for the container of content and added svg color

--- src/Experience/Experience.js
+++ src/Experience/Experience.js
@@ -1,15 +1,25 @@
-import React from 'react';
-import ExperienceSection from '../ExperienceSection/ExperienceSection'
-import ContactSection from '../Contact-Section/ContactSection'
-import './Experience.css'
+import React, { useState, useEffect } from 'react';
+import ExperienceSection from '../ExperienceSection/ExperienceSection';
+import ContactSection from '../Contact-Section/ContactSection';
+import './Experience.css';
 
-function Experience () {
+function Experience() {
+    const [isVisible, setIsVisible] = useState(false);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setIsVisible(true);
+        }, 3000); // 2000ms = 2 seconds delay
+
+        return () => clearTimeout(timer);
+    }, []);
+
     return (
-      <div className="flex experience-container">
-        <ExperienceSection number="01." text="Experience"/>
-        <ContactSection number="02." text="Contact"/>
-      </div>
+        <div className={`experience-container ${isVisible ? 'visible' : ''}`}>
+            <ExperienceSection number="01." text="Experience"/>
+            <ContactSection number="02." text="Contact"/>
+        </div>
     );
 }
 
-export default Experience
+export default Experience;
