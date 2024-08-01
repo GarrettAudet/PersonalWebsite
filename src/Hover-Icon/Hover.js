6c78c8fcd195c3958a9fbefe5c03bf6ec211ac00
GarrettAudet <70986283+GarrettAudet@users.noreply.github.com>
2024-07-31 21:53:26 -0400

    Added spinning animation to svgs

--- src/Hover-Icon/Hover.js
+++ src/Hover-Icon/Hover.js
@@ -7,14 +7,14 @@ import { ReactComponent as MediumLogo } from '../Img/medium.svg';
 function Hover() {
     return (
         <div className="socialContacts">
-            <a href = "https://github.com/GarrettAudet" title = "Github" className = "link">
-                <GithubLogo className = "bi bi-github bi-icon" />
+            <a href = "https://github.com/GarrettAudet" title = "Github" className = "link border">
+                <GithubLogo className = "bi-icon" />
             </a>
-            <a href = "https://www.linkedin.com/in/garrettaudet" title = "LinkedIn" className = "link">
-                <LinkedinLogo className = "bi bi-linkedin bi-icon"/>
+            <a href = "https://www.linkedin.com/in/garrettaudet" title = "LinkedIn" className = "link border">
+                <LinkedinLogo className = "bi-icon"/>
             </a>
-            <a href = "https://garrett-audet.medium.com/" title = "Medium" className = "link">
-                <MediumLogo className = "bi bi-medium bi-icon"/>
+            <a href = "https://garrett-audet.medium.com/" title = "Medium" className = "link border">
+                <MediumLogo className = "bi-icon"/>
             </a>
         </div>
     );
