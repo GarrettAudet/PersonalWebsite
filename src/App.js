87b84a712d2cd83e6a1fd58ffe9e76f296aabfd7
GarrettAudet <70986283+GarrettAudet@users.noreply.github.com>
2024-07-31 22:26:34 -0400

    Added cursor styling and functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './Header/Header'
 import Content from './Content/Content'
+import Cursor from './Cursor/Cursor'
 
 function App() {
   return (
     <div className="background">
       <Header />
       <Content />
+      <Cursor />
     </div>
   );
 }
