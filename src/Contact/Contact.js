b3c0462019572882006bc21bba8bebf65b470c8a
GarrettAudet <70986283+GarrettAudet@users.noreply.github.com>
2024-08-30 23:16:23 -0400

    Added form section

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -3,12 +3,31 @@ import './Contact.css';
 
 function ContactForm() {
     return (
-        <div className="ContactFormContainer">
-            <div className="SectionHeaderLine"></div>
-            <h1 className="SectionHeaderContact">Contact</h1>
-            <div className="SectionHeaderLine"></div>
+        <div className="contact-form-container">
+            <div className="contact-form-header">
+                <div className="contact-section-line"></div>
+                <h3 className="contact-section-title">Contact</h3>
+                <div className="contact-section-line"></div>
+            </div>
+            <form className="contact-form">
+                <div className="contact-form-name">
+                    <div className="contact-form-first-name">
+                        <input type="text" id="first-name" name="first-name" placeholder="First Name" required />
+                    </div>
+                    
+                    <div className="contact-form-last-name">
+                        <input type="text" id="last-name" name="last-name" placeholder="Last Name" required />
+                    </div>
+                </div>
+                <div className="form-group">
+                    <textarea id="message" name="message" rows="5" placeholder="Message" required></textarea>
+                </div>
+                
+                <button type="submit" className="submit-button">Submit</button>
+            </form>
         </div>
     );
 }
 
 export default ContactForm;
+
