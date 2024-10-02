591c9d568e760a334a36e7176002fef2f8fbaca8
GarrettAudet <70986283+GarrettAudet@users.noreply.github.com>
2024-10-02 01:21:51 -0400

    Merged button with a href in the contact form

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import './Contact.css';
+import { ReactComponent as GithubLogo } from '../Img/github.svg';
+import { ReactComponent as LinkedinLogo } from '../Img/linkedin.svg';
+import { ReactComponent as MediumLogo } from '../Img/medium.svg';
 
 function ContactForm() {
     return (
@@ -22,8 +25,20 @@ function ContactForm() {
                 <div className="form-group">
                     <textarea id="message" name="message" rows="5" placeholder="Message" required></textarea>
                 </div>
-                
-                <button type="submit" className="submit-button">Submit</button>
+                <div className="submit-button-container flex">
+                    <div className="socialContacts">
+                        <a href = "https://github.com/GarrettAudet" title = "Github" className = "link border">
+                            <GithubLogo className = "bi-icon" />
+                        </a>
+                        <a href = "https://www.linkedin.com/in/garrettaudet" title = "LinkedIn" className = "link border">
+                            <LinkedinLogo className = "bi-icon"/>
+                        </a>
+                        <a href = "https://garrett-audet.medium.com/" title = "Medium" className = "link border">
+                            <MediumLogo className = "bi-icon"/>
+                        </a>
+                    </div>
+                    <button type="submit" className="submit-button">Submit</button>
+                </div>
             </form>
         </div>
     );
