2fac537a604180230ef3b873985529697185f6a6
GarrettAudet <70986283+GarrettAudet@users.noreply.github.com>
2024-10-02 01:27:12 -0400

    Deleted the Hover files

--- src/Contact-Section/ContactSection.js
+++ src/Contact-Section/ContactSection.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './ContactSection.css';
-import Hover from '../Hover-Icon/Hover'
 import ContactForm from '../Contact/Contact';
 
 function ContactSection({ number, text }) {
@@ -8,7 +7,6 @@ function ContactSection({ number, text }) {
         <div className="ExperienceSection">
             <h3 className="flex sectionTitle contactTitle"><span>{number}&nbsp;</span> {text}</h3>
             <ContactForm/>
-            <Hover/>
         </div>
     );
 }
