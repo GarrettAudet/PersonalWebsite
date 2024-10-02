0946d7593f33878e8a4350c234be231cdfffbe06
GarrettAudet <70986283+GarrettAudet@users.noreply.github.com>
2024-10-02 02:25:10 -0400

    Fixed the line height problem with the titlecard

--- src/Titlecard/Titlecard.js
+++ src/Titlecard/Titlecard.js
@@ -7,8 +7,8 @@ const TitleCard = () => {
     <a href="https://www.linkedin.com/in/garrettaudet">
       <div className="header-container">
         <h2> 
-          <span className="inline-block appearIn">I'm a developer and business professional who believes that </span>
-          <span className="inline-block appearIn"><span className="text-decoration"> bridging diverse perspectives </span> unlocks <span className="text-animation">
+          <span className="inline-block appearIn flex">I'm a developer and business professional who believes that </span>
+          <span className="inline-block appearIn flex animation-container"><span className="text-decoration"> bridging diverse perspectives </span> unlocks <span className="text-animation">
             <TypeAnimation
               sequence={[
                 'economic potential.', 3000,
