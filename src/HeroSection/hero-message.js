24611c44fcdf3a3e24de765abbcc247375f05ed7
GarrettAudet <70986283+GarrettAudet@users.noreply.github.com>
2024-10-03 05:01:38 -0400

    Fixed font-message css in app.css

--- src/HeroSection/hero-message.js
+++ src/HeroSection/hero-message.js
@@ -6,7 +6,7 @@ const HeroMessage = () => {
   return (
     <a href="https://www.linkedin.com/in/garrettaudet">
       <div className="header-container">
-        <h2> 
+        <h2 className="hero-message-container"> 
           <span className="inline-block appearIn flex">I'm a developer and business professional who believes that </span>
           <span className="inline-block appearIn flex animation-container"><span className="text-decoration"> bridging diverse perspectives </span> unlocks <span className="text-animation">
             <TypeAnimation
