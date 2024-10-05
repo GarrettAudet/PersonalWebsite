b4428aa8efe5fa2870fd8b1c6b756500a8ec72e6
GarrettAudet <70986283+GarrettAudet@users.noreply.github.com>
2024-10-04 22:45:58 -0400

    Added particles.js library

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './Header/Header'
 import Content from './Content/Content'
 import Cursor from './Cursor/Cursor'
+import LandingPage from './LandingPage/landing-page'
 
 function App() {
   const experienceRef = useRef(null);
@@ -13,7 +14,8 @@ function App() {
   };
 
   return (
-    <div className="background">
+    <div className="scrolling-container">
+      <LandingPage/>
       <Header scrollToSection={scrollToSection} experienceRef={experienceRef} contactRef={contactRef}/>
       <Content experienceRef={experienceRef} contactRef={contactRef}/>
       <Cursor />
---
+++ src/LandingPage/landing-page.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './landing-page.css';
+
+const LandingPage = () => {
+  return (
+    <div className = "scroll-section particles-js">
+
+    </div>
+  );
+};
+
+export default LandingPage;
